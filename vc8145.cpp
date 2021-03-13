a1d33b4c977293776c36aa2f0f4fbaa198e6aaab
AdamC19 <cordingleyadam19+github@gmail.com>
2021-03-13 12:01:43 -0500

    Initial Commit take 2

--- vc8145.cpp
+++ vc8145.cpp
@@ -1,6 +1,13 @@
 #include "vc8145.h"
+#include <QSerialPort>
 
-VC8145::VC8145(QObject *parent) : QObject(parent)
+VC8145::VC8145(QObject *parent) : QObject(parent), serial(parent)
 {
 
+}
+
+bool VC8145::connect(QString comport){
+    serial.setPortName(comport);
+    serial.setBaudRate(VC8145_DEFAULT_BAUD_RATE);
+    return true;
 }
--- vc8145.h
+++ vc8145.h
@@ -2,13 +2,19 @@
 #define VC8145_H
 
 #include <QObject>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+
+#define VC8145_DEFAULT_BAUD_RATE QSerialPort::Baud9600
 
 class VC8145 : public QObject
 {
     Q_OBJECT
+private:
+    QSerialPort serial;
 public:
     explicit VC8145(QObject *parent = nullptr);
-
+    bool connect(QString comport);
 signals:
 
 };
