6c8cd3de3128baad0ada8b7535a7219984a27f67
AdamC19 <cordingleyadam19+github@gmail.com>
2021-03-13 12:00:57 -0500

    Initial Commit

---
+++ vc8145.cpp
@@ -0,0 +1,6 @@
+#include "vc8145.h"
+
+VC8145::VC8145(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ vc8145.h
@@ -0,0 +1,16 @@
+#ifndef VC8145_H
+#define VC8145_H
+
+#include <QObject>
+
+class VC8145 : public QObject
+{
+    Q_OBJECT
+public:
+    explicit VC8145(QObject *parent = nullptr);
+
+signals:
+
+};
+
+#endif // VC8145_H
